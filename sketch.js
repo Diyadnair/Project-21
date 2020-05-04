d326ae952801c931c5c3666a418d05ea3af04976
Diyadnair <64205177+Diyadnair@users.noreply.github.com>
2020-05-04 19:46:35 +0530

    Add files via upload
    wall change color when the bullet hit the wall

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var bullet, speed,weight;
+var wall,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet =  createSprite(50, 200, 50, 10);
+  bullet.shapeColor = "white" ;
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+}
+
+function draw() {
+  background(0,0,0);
+  bullet.velocityX = speed;
+ bullet.collide(wall);
+ if(hasCollided(bullet,wall))
+ {
+bullet.velocityX = 0;
+var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness); 
+if(damage>10)
+{
+wall.shapeColor = color(255,0,0);
+}
+if(damage<10)
+{
+bwall.shapeColor = color(0,255,0);
+}
+
+ }
+  drawSprites();
+}
+
+function hasCollided(bullet1,wall1){
+  bulletRightEdge = bullet1.x + bullet1.width;
+  wallLeftEdge = wall1.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+return true;
+  }
+  return false;
+}
